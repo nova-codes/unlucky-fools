8dbace4c174dc83888589264b2abfdc6ffd03d01
Hannah Elliott <hgelliott90@gmail.com>
2021-05-07 15:28:15 -0400

    Creating input component.

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,15 +1,33 @@
+import React from 'react';
 import './Input.css';
 
-const Input = () => {
-    return (
-        <div className='input-container'>
-            <h1 className='uf-title'> Enter Their Names </h1>
+class Input extends React.Component {
+    state = {
+        nameValue: []
+    };
 
-            <input id='names' className='input-field' type='text' placeholder='Each name separated by a comma' />
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
 
-            <button id='fate' className='he-btn btn-round btn-indigo'>seal their fate</button>
-        </div>
-    )
+    handleSubmit(event) {
+        console.log('Submitted! Value: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className='input-container'>
+                <h1 className='uf-title'> Enter Their Names </h1>
+                
+                <form onSubmit={this.handleSubmit}>
+                    <input id='names' className='input-field' type='text' placeholder='Each name separated by a comma' value={this.state.nameValue} onChange={this.handleChange} />
+
+                    <button id='fate' className='he-btn btn-round btn-indigo'>seal their fate</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Input;
